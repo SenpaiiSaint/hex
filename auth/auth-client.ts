70cbe77a3f39e7d764ff935a26efd3bf3aa6aa86
Sharif <saintparish6@gmail.com>
2025-06-14 15:33:01 -0400

    Update Auth Client

--- auth/auth-client.ts
+++ auth/auth-client.ts
@@ -1,11 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
-const baseURL = typeof window !== 'undefined' 
-  ? `${window.location.origin}/api/auth`
-  : "https://hex-phi.vercel.app/api/auth";
-
 export const authClient = createAuthClient({
-    baseURL,
+    baseURL: "https://hex-phi.vercel.app/api/auth",
 })
 
 export const revokeSession = async (token: string) => await authClient.revokeSession({ token });
