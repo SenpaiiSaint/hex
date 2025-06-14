4c00a719b59b3fa81015579a28e1fc3049415914
Sharif <saintparish6@gmail.com>
2025-06-14 15:46:46 -0400

    Schema Fix

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,6 +1,6 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from '../generated/prisma'
 import { resend } from "../lib/resend";
 
 const prisma = new PrismaClient();
